1b416e5cb397a55c6e95a9e4f9319bee0635942f
Ramon Casagrande <ramoncasagrande@hotmail.com>
2023-12-20 17:09:18 -0300

    Instalado CORS

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
 const alunoRoute = require("./src/routes/aluno.route");
+const cors = require('cors');
 const connectDatabase = require("./src/database/db");
 const port = 3000;
 
 connectDatabase();
+app.use(cors());
 app.use(express.json());
 app.use('/aluno', alunoRoute);
 
