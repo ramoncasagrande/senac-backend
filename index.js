99ab33f48aa4919c36166f05b0c287c907a9deb4
Ramon Casagrande <ramoncasagrande@hotmail.com>
2023-12-20 13:06:35 -0300

    Criada conexão com o banco

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 const alunoRoute = require("./src/routes/aluno.route");
+const connectDatabase = require("./src/database/db");
 const port = 3000;
 
+connectDatabase();
 app.use(express.json());
 app.use('/aluno', alunoRoute);
 
--- src/database/db.js
+++ src/database/db.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 
 const connectDatabase = () => {
     console.log("Aguardando conexão...");
-    mongoose.connect('mongodb://127.0.0.1:27017/senac');
+    mongoose.connect('mongodb://127.0.0.1:27017/senac').then(() => {console.log("MongoDB Conectado!")})
+    .catch((erro) => {console.log(erro)});
 }
 
 module.exports = connectDatabase;
