74136f8f3e2f3d366277351842b40f550befa76b
Ramon Casagrande <ramoncasagrande@hotmail.com>
2023-12-20 13:22:27 -0300

    Criada função  async no controller

--- src/controllers/aluno.controller.js
+++ src/controllers/aluno.controller.js
@@ -1,8 +1,16 @@
-const post = (req, res) => {
+const alunoService = require('../services/aluno.service')
+
+const post = async (req, res) => {
     const { email, nome } = req.body;
 
     if(!email || !nome){
         res.status(400).send({message:'preencha os campos corretamente'});
+    }
+
+    const aluno = await alunoService.post(req.body);
+
+    if(!aluno){
+        return res.status(400).send({message: "Erro na criação do Aluno"});
     }
 
     res.status(200).send({
